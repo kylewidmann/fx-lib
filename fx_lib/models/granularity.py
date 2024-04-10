51b25f113cdae76c2e28ff9f5bbaa1cb1d9eafbf
Kyle Widmann <Kyle.Widmann@gmail.com>
2024-04-10 08:15:46 -0400

    Adding granularity and types

---
+++ fx_lib/models/granularity.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Granularity(Enum):
+    M1 = "M1"
+    M5 = "M5"
+    M15 = "M15"
---
+++ fx_lib/models/granularity.pyi
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Granularity(Enum):
+    M1: str
+    M5: str
+    M15: str
